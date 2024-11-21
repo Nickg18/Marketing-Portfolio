87c5899dd837d97d087a4fdea55084cf3f48e230
nickg <nickegelineau@gmail.com>
2024-11-21 08:34:40 -0500

    navigation finish

--- portfolio-website/index.js
+++ portfolio-website/index.js
@@ -1,7 +1,9 @@
 let navToggle = document.querySelector('.burger');
+let links = document.querySelector('.links');
 
 function toggleHamburger() {
     navToggle.classList.toggle('active');
+    links.classList.toggle('activate');
 }
 
 navToggle.addEventListener('click', toggleHamburger);
